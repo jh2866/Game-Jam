fb589ab451529a41889a52a66cfa20d47c55130e
Joe hall <jh2866@msstate.edu>
2017-03-02 22:17:41 -0600

    Added Game

---
+++ Assets/menuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuScript : MonoBehaviour 
+{
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+	}
+	
+	public void StartLevel()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+}
